09fb04de5d8a9c127c9193bc13aa999fd5c2c53b
Rodrigo Delgado <rdalcega@gmail.com>
2015-09-20 11:09:02 -0700

    Created paths.js file to request absolute paths in server

---
+++ paths.js
@@ -0,0 +1,21 @@
+var path = require( 'path' );
+
+module.exports = {
+
+  samples: path.join(
+
+    __dirname,
+
+    'client/samples'
+
+  ),
+
+  client: path.join(
+
+    __dirname,
+
+    'client'
+
+  )
+
+};
