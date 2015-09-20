1d826791390d0a18fa21f370e3ebc8d51572bd6f
Rodrigo Delgado <rdalcega@gmail.com>
2015-09-20 11:19:19 -0700

    created playSound helper function

---
+++ client/helpers/playSound.js
@@ -0,0 +1,13 @@
+var playSound = function( buffer, offset ) {
+
+  offset = offset || 0;
+
+  var source = context.createBufferSource( );
+
+  source.buffer = buffer;
+
+  source.connect = context.destination;
+
+  source.start( offset );
+
+};
